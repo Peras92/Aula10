8bb748ddc97976e7c3f157dae0b7206a295fc828
Rui Pereira <ruisilvapereira1@gmail.com>
2021-04-24 16:35:21 +0100

    Agora com loop

--- Idades.py
+++ Idades.py
@@ -1,10 +1,8 @@
 with open("Idades.txt", "r") as info:
     linhas = info.read().splitlines()
+    
+    for i in range(len(linhas)):
+        por_linha = linhas[i].split(",")
 
-    row_data0 = linhas[0].split(",")
-    row_data1 = linhas[1].split(",")
-    row_data2 = linhas[2].split(",")
+        print(f"{por_linha[0]} tem {por_linha[1]} anos de idade e é {por_linha[2]}" + "\n")
 
-    print("{0} tem {1} anos de idade e é {2}".format(row_data0[0], row_data0[1], row_data0[2]) + "\n")
-    print("{0} tem {1} anos de idade e é {2}".format(row_data1[0], row_data1[1], row_data1[2]) + "\n")
-    print("{0} tem {1} anos de idade e é {2}".format(row_data2[0], row_data2[1], row_data2[2]) + "\n")
